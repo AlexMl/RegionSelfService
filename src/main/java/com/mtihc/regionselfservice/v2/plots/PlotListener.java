b764bbbb0df18b06a2c55aac7322ba46799131ec
AlexMl <alexandermlinzk@gmail.com>
2015-10-06 17:23:54 +0200

    Dont drop signs if player is in creative mode.

--- src/main/java/com/mtihc/regionselfservice/v2/plots/PlotListener.java
+++ src/main/java/com/mtihc/regionselfservice/v2/plots/PlotListener.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
@@ -74,21 +75,14 @@ class PlotListener implements Listener {
 	    // add sign to plot... later
 	    
 	} catch (SignException e) {
-	    // invalid sign
-	    player.sendMessage(ChatColor.RED + "Failed to create " + type.name() + " sign: ");
-	    player.sendMessage(ChatColor.RED + e.getMessage());
-	    // break the sign
-	    event.setCancelled(true);
-	    sign.getBlock().breakNaturally();
+	    // invalid sign Cancel the event
+	    denieSignChange(player, ChatColor.RED + "Failed to create " + type.name() + " sign:\n" + e.getMessage(), sign.getBlock(), true, event, true);
 	    return;
 	}
 	
 	ProtectedRegion region = plot.getRegion();
 	if (region == null) {
-	    player.sendMessage(ChatColor.RED + "Failed to create " + type.name() + " sign. ");
-	    player.sendMessage(ChatColor.RED + "Region \"" + plot.getRegionId() + "\" doesn't exist.");
-	    event.setCancelled(true);
-	    sign.getBlock().breakNaturally();
+	    denieSignChange(player, ChatColor.RED + "Failed to create " + type.name() + " sign.\nRegion \"" + plot.getRegionId() + "\" doesn't exist.", sign.getBlock(), true, event, true);
 	    return;
 	}
 	
@@ -101,25 +95,19 @@ class PlotListener implements Listener {
 	    
 	    // check permission to rent out
 	    if (!player.hasPermission(Permission.RENTOUT)) {
-		player.sendMessage(ChatColor.RED + "You don't have permission to rent out regions.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "You don't have permission to rent out regions.", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
 	    // check permission to rent out, unowned regions
 	    if (!isOwner && !player.hasPermission(Permission.RENTOUT_ANYREGION)) {
-		player.sendMessage(ChatColor.RED + "You can't rent out regions that you don't own.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "You can't rent out regions that you don't own.", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
 	    // check permission to rent out, outside the region
 	    if (!isInside && !player.hasPermission(Permission.RENTOUT_ANYWHERE)) {
-		player.sendMessage(ChatColor.RED + "You can't place this sign outside the region itself.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "You can't place this sign outside the region itself.", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
@@ -127,9 +115,7 @@ class PlotListener implements Listener {
 	    // check if is rented out, then player typed a name on the sign
 	    // instead of cost
 	    if (rentText.isRentedOut()) {
-		player.sendMessage(ChatColor.RED + "Invalid sign text. Expected rent-cost and rent-time.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "Invalid sign text. Expected rent-cost and rent-time.", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
@@ -152,27 +138,25 @@ class PlotListener implements Listener {
 	    double minRentCostConverted = minRentCost * (rentTime / (1000.0 * 60.0 * 60.0));
 	    
 	    if (rentCost < minRentCostConverted) {
-		player.sendMessage(ChatColor.RED + "The price is too low.");
-		player.sendMessage(ChatColor.RED + "The rent-price must be between " + this.mgr.getEconomy().format(minRentCostConverted) + " and " + this.mgr.getEconomy().format(maxRentCostConverted) + " per " + rentTimeString + ".");
-		player.sendMessage(ChatColor.RED + "In other words, between " + this.mgr.getEconomy().format(minRentCost) + " and " + this.mgr.getEconomy().format(maxRentCost) + " per hour.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		StringBuilder string = new StringBuilder();
+		string.append(ChatColor.RED + "The price is too low.\n");
+		string.append("The rent-price must be between " + this.mgr.getEconomy().format(minRentCostConverted) + " and " + this.mgr.getEconomy().format(maxRentCostConverted) + " per " + rentTimeString + ".\n");
+		string.append("In other words, between " + this.mgr.getEconomy().format(minRentCost) + " and " + this.mgr.getEconomy().format(maxRentCost) + " per hour.");
+		denieSignChange(player, string.toString(), sign.getBlock(), true, event, true);
 		return;
 	    } else if (rentCost > maxRentCostConverted) {
-		player.sendMessage(ChatColor.RED + "The price is too high.");
-		player.sendMessage(ChatColor.RED + "The rent-price must be between " + this.mgr.getEconomy().format(minRentCostConverted) + " and " + this.mgr.getEconomy().format(maxRentCostConverted) + " per " + rentTimeString + ".");
-		player.sendMessage(ChatColor.RED + "In other words, between " + this.mgr.getEconomy().format(minRentCost) + " and " + this.mgr.getEconomy().format(maxRentCost) + " per hour.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		StringBuilder string = new StringBuilder();
+		string.append(ChatColor.RED + "The price is too high!\n");
+		string.append("The rent-price must be between " + this.mgr.getEconomy().format(minRentCostConverted) + " and " + this.mgr.getEconomy().format(maxRentCostConverted) + " per " + rentTimeString + ".\n");
+		string.append("In other words, between " + this.mgr.getEconomy().format(minRentCost) + " and " + this.mgr.getEconomy().format(maxRentCost) + " per hour.");
+		denieSignChange(player, string.toString(), sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
 	    // check permission to rent out, for free
 	    if (rentCost == 0) {
 		if (!player.hasPermission(Permission.RENTOUT_FREE)) {
-		    player.sendMessage(ChatColor.RED + "You don't have permission to rent out regions for free.");
-		    event.setCancelled(true);
-		    sign.getBlock().breakNaturally();
+		    denieSignChange(player, ChatColor.RED + "You don't have permission to rent out regions for free.", sign.getBlock(), true, event, true);
 		    return;
 		}
 	    }
@@ -188,17 +172,13 @@ class PlotListener implements Listener {
 	    
 	    // check permission to sell
 	    if (!player.hasPermission(Permission.SELL)) {
-		player.sendMessage(ChatColor.RED + "You don't have permission to sell regions.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "You don't have permission to sell regions.", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
 	    // check permission to sell, unowned regions
 	    if (!isOwner && !player.hasPermission(Permission.SELL_ANYREGION)) {
-		player.sendMessage(ChatColor.RED + "You can't sell regions that you don't own.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		
 		return;
 	    }
 	    
@@ -221,9 +201,7 @@ class PlotListener implements Listener {
 	    
 	    // check permission to sell, outside the region
 	    if (!isInside && !player.hasPermission(Permission.SELL_ANYWHERE)) {
-		player.sendMessage(ChatColor.RED + "You can't place this sign outside the region itself.");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "You can't place this sign outside the region itself.", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
@@ -237,25 +215,17 @@ class PlotListener implements Listener {
 	    double maxSellCost = plot.getWorth(config.getOnSellMaxBlockCost());
 	    
 	    if (sellCost < minSellCost) {
-		player.sendMessage(ChatColor.RED + "The price is too low.");
-		player.sendMessage(ChatColor.RED + "The sell-cost must be between " + this.mgr.getEconomy().format(minSellCost) + " and " + this.mgr.getEconomy().format(maxSellCost) + ".");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "The price is too low.\nThe sell-cost must be between " + this.mgr.getEconomy().format(minSellCost) + " and " + this.mgr.getEconomy().format(maxSellCost) + ".", sign.getBlock(), true, event, true);
 		return;
 	    } else if (sellCost > maxSellCost) {
-		player.sendMessage(ChatColor.RED + "The price is high.");
-		player.sendMessage(ChatColor.RED + "The sell-cost must be between " + this.mgr.getEconomy().format(minSellCost) + " and " + this.mgr.getEconomy().format(maxSellCost) + ".");
-		event.setCancelled(true);
-		sign.getBlock().breakNaturally();
+		denieSignChange(player, ChatColor.RED + "The price is too high.\nThe sell-cost must be between " + this.mgr.getEconomy().format(minSellCost) + " and " + this.mgr.getEconomy().format(maxSellCost) + ".", sign.getBlock(), true, event, true);
 		return;
 	    }
 	    
 	    // check permission to sell, for free
 	    if (sellCost == 0) {
 		if (!player.hasPermission(Permission.SELL_FREE)) {
-		    player.sendMessage(ChatColor.RED + "You don't have permission to sell regions for free.");
-		    event.setCancelled(true);
-		    sign.getBlock().breakNaturally();
+		    denieSignChange(player, ChatColor.RED + "You don't have permission to sell regions for free.", sign.getBlock(), true, event, true);
 		    return;
 		}
 	    }
@@ -301,27 +271,38 @@ class PlotListener implements Listener {
 				return;
 			    } else {
 				// protected region doesn't exist
-				event.getPlayer().sendMessage(ChatColor.RED + "Sorry, region '" + plot.getRegionId() + "' doesn't exist anymore.");
-				event.getClickedBlock().breakNaturally();
+				denieSignChange(event.getPlayer(), ChatColor.RED + "Sorry, region '" + plot.getRegionId() + "' doesn't exist anymore.", event.getClickedBlock(), true, event, false);
 				plot.delete();
 				return;
 			    }
 			} else {
 			    // didn't find the plot information
-			    event.getPlayer().sendMessage(ChatColor.RED + "Sorry, this sign is invalid. Couldn't find the plot information.");
-			    event.getClickedBlock().breakNaturally();
+			    denieSignChange(event.getPlayer(), ChatColor.RED + "Sorry, this sign is invalid. Couldn't find the plot information.", event.getClickedBlock(), true, event, false);
 			    return;
 			}
 		    } catch (SignException e) {
 			// not a valid sign
-			event.getPlayer().sendMessage(ChatColor.RED + e.getMessage());
-			event.getClickedBlock().breakNaturally();
+			denieSignChange(event.getPlayer(), ChatColor.RED + e.getMessage(), event.getClickedBlock(), true, event, false);
 			return;
 		    }
 		}
 	    }
 	}
 	
+    }
+    
+    private void denieSignChange(Player eventPlayer, String playerMessage, Block block, boolean shouldBreak, Cancellable event, boolean cancelEvent) {
+	
+	if (shouldBreak) {
+	    if (eventPlayer.getGameMode() != GameMode.CREATIVE) {
+		block.breakNaturally();
+	    }
+	}
+	
+	if (cancelEvent) {
+	    event.setCancelled(true);
+	}
+	eventPlayer.sendMessage(playerMessage);
     }
     
     @EventHandler
